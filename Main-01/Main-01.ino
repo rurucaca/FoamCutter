dffd93a083d68ced25b8213091b9e674a709c0cc
X240\pond_ <sleudkanjana@gmail.com>
2018-03-28 18:42:23 +0700

    add project file

---
+++ Main-01/Main-01.ino
@@ -0,0 +1,12 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
